f95f96dab8bbfd5c30936bc6cfcf67e4dab29eb5
Em Ha Tuan <108352933+emhat098@users.noreply.github.com>
2022-12-26 21:14:50 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,74 @@
-const myObject = [
-    {
-        id: 1,
-        name: 'javascript',
-        coin: 12,
-    },
+const listCoin = [
+  {
+      id: 1,
+      name: 'javascript',
+      coin: 12,
+  },
 
-    {
-        id: 2,
-        name: 'html',
-        coin: 65,
-    },
-    
-    {
-        id: 3,
-        name: 'css',
-        coin: 125,
-    },
+  {
+      id: 2,
+      name: 'html',
+      coin: 65,
+  },
+  
+  {
+      id: 3,
+      name: 'css',
+      coin: 125,
+  },
 
-    {
-        id: 4,
-        name: 'python',
-        coin: 54,
-    },
+  {
+      id: 4,
+      name: 'python',
+      coin: 54,
+  },
 
-    {
-        id: 5,
-        name: 'dart',
-        coin: 120,
-    },
+  {
+      id: 5,
+      name: 'dart',
+      coin: 120,
+  },
 
-    {
-        id: 6,
-        name: 'ruby',
-        coin: 56,
-    },
+  {
+      id: 6,
+      name: 'ruby',
+      coin: 56,
+  },
 ]
 
-var coincost = 100
-
 const dung = function() {
-    for(var i = 0; i < myObject.length; i++){
-        if(myObject[i].id % 2 === 0){
-            coincost += myObject[i].coin
-            return coincost
-        }
-        else{
-            return 'wrong'
-        }
+  for(let i = 0; i < listCoin.length; i++) {
+    let coincost = 100;
+    if(listCoin[i].id % 2 === 0){
+      coincost += listCoin[i].coin
+      return coincost;
+    } else{
+      return 'wrong';
     }
+  }
 }
 
-console.log(dung())
+// Clone map.
+// const newListCoin = listCoin.map((item) => {
+//   let coincost = 100;
+//   if (item.id % 2 === 0) {
+//     return coincost += item.coin;
+//   }
+//   return 'wrong';
+// });
+
+const cloneMap = () => {
+  let newList = [];
+  for(let i = 0; i < listCoin.length; i++) {
+    let coincost = 100;
+    if (listCoin[i].id % 2 === 0) {
+      newList.push(coincost += listCoin[i].coin);
+    } else{
+      newList.push('wrong');
+    }
+  }
+  return newList;
+} 
+
+const newListCoin = cloneMap();
+console.log(newListCoin);
